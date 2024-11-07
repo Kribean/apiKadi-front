5afea7026037834e6ebbd74067f40728ca1f1dd6
Kribean <caraibe.simulation@gmail.com>
2024-11-07 21:08:51 +0100

    feat: wip integrate api

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,25 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import ProductContainer from "@/components/ProductContainer";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+
+    const products = await prisma.product.findMany({
+      orderBy: {
+        createdAt: 'desc', // Trie par date de création décroissante
+      },
+      take: 50, // Limite à 50 articles
+    });
+      // Remplacer les valeurs nulles de price_per_unit et price_per_kilo par une valeur par défaut
+  const processedProducts = products.map(product => ({
+    ...product,
+    price_per_unit: product.price_per_unit ?? "0", // Remplace null par "0" ou une autre valeur par défaut
+    price_per_kilo: product.price_per_kilo ?? "0", // Pareil pour price_per_kilo
+  }));
   return (
     <div className="flex flex-col items-center justify-between min-h-screen font-[family-name:var(--font-geist-sans)]">
+      {processedProducts?.map((e)=>{return <p>{e.nameArticle}</p>})}
       <Navbar />
       <Hero />
       <main className="flex flex-col m-[0px] p-[0px] w-full ">
